527182064daf8b65638acb5921bbe0769c94fc4b
Andrew Pungprakearti <andrew@alephsf.com>
2023-01-18 18:16:14 -0800

    WIP grid and container numbers

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-const Page404 = () => {
-  return (
-    <div>
-      404 - Page Not Found
-    </div>
-  )
-}
-
-export default Page404
---
+++ pages/404.tsx
@@ -0,0 +1,5 @@
+const Page404 = () => {
+  return <div>404 - Page Not Found</div>
+}
+
+export default Page404
